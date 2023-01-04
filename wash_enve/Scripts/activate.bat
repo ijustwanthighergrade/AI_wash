a1c38cb6723a5782103e71ef01c4aa26d1902eec
10944143 <92848136+10944143@users.noreply.github.com>
2023-01-04 13:01:28 +0800

    footer更新

--- wash_enve/Scripts/activate.bat
+++ wash_enve/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\DAIYUNWU\Desktop\AI_wash\wash_enve
+set VIRTUAL_ENV=C:\Users\Amy\Desktop\AI_wash\wash_enve
 
 if not defined PROMPT set PROMPT=$P$G
 
