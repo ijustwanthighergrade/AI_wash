b2266cd9ee5f9b47ce14121d6ab33f90b6056be4
ijustwanthighergrade <gill998546@gmail.com>
2023-01-11 15:38:39 +0800

    記住資訊 預設資料

--- wash/procedure/views.py
+++ wash/procedure/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import sqlite3
+from django.template import Template,Context
 from login.views import login2
 from procedure.models import order
 from procedure.models import WMODE
@@ -11,6 +12,7 @@ from procedure.models import delivery
 from procedure.models import shop
 from procedure.models import bag
 from procedure.models import lock
+from login.models import MEMBER
 
 orderdata = {
    'Take': '',
@@ -25,7 +27,11 @@ orderdata = {
    'getdate':'',
    'gettime':'',
    'mphone':'',
-   'creditcard':''
+   'creditcard':'',
+   'usetime': 0,
+   'tax': 0,
+   
+   
 }
 
 def wash1(request):
@@ -80,6 +86,22 @@ def wash3(request):
          orderdata['dryway']=drying
          orderdata['flodway']=store
          orderdata['specialitem']=special
+         
+         
+      wmode=WMODE.objects.get(WMODE=orderdata['washway'])
+      lmode=LMODE.objects.get(LMODE=orderdata['dryway'])
+      fmode=FMODE.objects.get(FMODE=orderdata['flodway'])
+      
+      PRICE = wmode.MONEY + lmode.MONEY + fmode.MONEY + int(orderdata['bagamount']) * 50 + wmode.MEMISSIONS * 3 + lmode.MEMISSIONS * 3 + fmode.MEMISSIONS * 3
+      GPOINT = wmode.POINTS + lmode.POINTS + fmode.POINTS+ int(orderdata['bagamount']) * 20
+      C_AMOUNT = wmode.MEMISSIONS * 3 + lmode.MEMISSIONS * 3 + fmode.MEMISSIONS * 3
+      
+      orderdata['tpoint']=GPOINT
+      orderdata['tprice']=PRICE
+      orderdata['tax']=C_AMOUNT
+      
+      
+      
       
       print(orderdata)
       return render(request,"wash3.html",locals())
@@ -95,20 +117,53 @@ def wash4(request):
       orderdata['getdate']=day
       orderdata['gettime']=time
       
+      orderamount = orderdata['bagamount']
+      washway= orderdata['washway']
+      dryway= orderdata['dryway']
+      flodway= orderdata['flodway']
+      specialitem= orderdata['specialitem']
+      tpoint= orderdata['tpoint']
+      tprice= orderdata['tprice']
+      tax=orderdata['tax']
+      
+      date=orderdata['getdate']
+      time=orderdata['gettime']
+      takeway=orderdata['Take']
+      t=""
+      if takeway=='外送':
+         t=orderdata['addr']
+      else:
+         t="商家自取"
+         
+      mem = MEMBER.objects.get(MEMID=request.session['mem_session'])
+      phone=mem.MEMPHONE
+      card=mem.MEMCARD
+      
+      
       print(orderdata)
       return render(request,"wash4.html",locals())
    else:
       return login2(request)
 
 def dealorder(request):
+   rememberphone=""
+   remembercard=""
    if 'mem_session' in request.session:
       if request.method == 'POST':
          phone=request.POST.get('phone')
          card=request.POST.get('card')
+         rememberphone=request.POST.get('rememberphone')
+         remembercard=request.POST.get('remembercard')
          
       orderdata['mphone']=phone,
       orderdata['creditcard']=card
-   
+      
+   mem = MEMBER.objects.filter(MEMID=request.session['mem_session'])
+   if rememberphone != "":
+      mem.update(MEMPHONE=phone)
+   if remembercard != "":
+      mem.update(MEMCARD=card)
+      
    
    
    WASH=WMODE.objects.get(WMODE=orderdata['washway'])
@@ -123,7 +178,9 @@ def dealorder(request):
    order.objects.filter(ORDID=ORD).update(MEMID=request.session['mem_session'])
    shops=shop.objects.get(id=1)
    SHOPS=shops.SHOPID
-   delivery.objects.create(ORDID=ORD,SHOPID=SHOPS,PHONE=orderdata['mphone'], ADDRESS=orderdata['addr'])
+   
+   if(orderdata['Take']=="外送"):
+      delivery.objects.create(ORDID=ORD,SHOPID=SHOPS,PHONE=orderdata['mphone'], ADDRESS=orderdata['addr'])
    
    
    print(orderdata)
