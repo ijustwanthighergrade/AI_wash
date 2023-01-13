4801e0400afa4583688a739d71b4d316904f8970
Rourourourou <nekokage20011201@gmail.com>
2023-01-13 18:24:33 +0800

    改顏色

--- wash/procedure/views.py
+++ wash/procedure/views.py
@@ -125,6 +125,9 @@ def wash3(request):
       lmode=LMODE.objects.get(LMODE=orderdata['dryway'])
       fmode=FMODE.objects.get(FMODE=orderdata['flodway'])
       
+      totaltime=wmode.TIME+fmode.TIME+lmode.TIME
+      
+      
       PRICE = wmode.MONEY + lmode.MONEY + fmode.MONEY + int(orderdata['bagamount']) * 50 + wmode.MEMISSIONS * 3 + lmode.MEMISSIONS * 3 + fmode.MEMISSIONS * 3
       GPOINT = wmode.POINTS + lmode.POINTS + fmode.POINTS+ int(orderdata['bagamount']) * 20
       C_AMOUNT = wmode.MEMISSIONS * 3 + lmode.MEMISSIONS * 3 + fmode.MEMISSIONS * 3
@@ -221,16 +224,17 @@ def dealorder(request):
    if(orderdata['Take']=="外送"):
       delivery.objects.create(ORDID=ORD,SHOPID=SHOPS,PHONE=orderdata['mphone'], ADDRESS=orderdata['addr'],GDATE=datechange)
       
-   # a=order.objects.get(ORDID=ORD)
-   # res = requests.get("http://127.0.0.1:5000/add", json = {
-   #    "ORDID": a.ORDID,
-   #    "MEMID": a.MEMID,
-   #    "CDATE": a.CDATE.strftime("%Y-%m-%d %H:%M"),
-   #    "GPOINT": a.GPOINT,
-   #    "AMOUNT": a.AMOUNT,
-   #    "APPID": a.APPID
-   # })
-   
+   a=order.objects.get(ORDID=ORD)
+   data =  {
+      "ORDID": a.ORDID,
+      "MEMID": a.MEMID,
+      "CDATE": a.CDATE.strftime("%Y-%m-%d %H:%M"),
+      "GPOINT": a.GPOINT,
+      "AMOUNT": a.AMOUNT,
+      "APPID": a.APPID
+   }
+   res = requests.post('https://a6b2-59-124-157-21.jp.ngrok.io/api/myapp/', data,headers = {'ngrok-skip-browser-warning': '7414'} )
+
    print(orderdata)
 
    mycolor=COLOR.objects.get(MEMID=request.session['mem_session']).WHICHCOLOR
